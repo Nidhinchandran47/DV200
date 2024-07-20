da197b318b739c31dabdc9cd71748b587b73db64
Nidhin Chandran <nidhinchandran470@gmail.com>
2024-07-20 21:08:07 +0530

    modified fifo to async

--- DESIGNS/5. Memory/3. FIFO/fifo.v
+++ DESIGNS/5. Memory/3. FIFO/fifo.v
@@ -1,5 +1,5 @@
 module fifo (
-    input clk, rst, we, re,
+    input clkr,clkw, rst, we, re,
     input [7:0] data_in,
     output empty, full,
     output reg [7:0] data_out
@@ -8,11 +8,10 @@ module fifo (
     reg [4:0] w_ptr, r_ptr;
     integer i;
 
-    always @(posedge clk) begin
+    always @(posedge clkw) begin
         if (rst) begin
             for(i=0; i<16; i=i+1)
                 mem[i] <= 0;
-            data_out <= 0;
             w_ptr <= 0;
             r_ptr <= 0;
         end
@@ -23,11 +22,22 @@ module fifo (
             end
             else
                 w_ptr <= w_ptr;
+        end
+    end
+
+    always @(posedge clkr) begin
+        if (rst) begin
+            data_out <= 0;
+        end
+        else begin
             if (re && !empty) begin
                 data_out <= mem[r_ptr[3:0]];
                 r_ptr <= r_ptr +1;
             end
+            else
+                r_ptr <= r_ptr;
         end
+        
     end
 
     assign empty = (w_ptr == r_ptr);
--- DESIGNS/5. Memory/3. FIFO/fifo_tb.v
+++ DESIGNS/5. Memory/3. FIFO/fifo_tb.v
@@ -1,2 +1,87 @@
-module
-    
+module fifo_tb();
+    reg clkr,clkw, rst, we, re;
+    reg [7:0] data_in;
+    wire empty, full;
+    wire [7:0] data_out;
+    parameter PHASE = 2;
+    parameter CYCLE =10;
+    integer i;
+    fifo dut (clkr, clkw, rst, we, re,data_in, empty, full, data_out);
+
+    always begin
+        clkr = 1'b0;
+        #(CYCLE/2);
+        forever begin
+            clkr = ~clkr;
+            #(CYCLE/2);
+        end
+    end
+
+    always begin
+        #PHASE;
+        clkw = 1'b0;
+        #(CYCLE/2);
+        forever begin
+            clkw = ~clkw;
+            #(CYCLE/2);
+        end
+    end
+
+    task initialize(); begin
+            @(negedge clkw);
+            rst = 1'b1;
+            @(negedge clkw);
+            rst = 1'b0;
+        end
+    endtask
+
+    task write(input[7:0]i); begin
+        @(negedge clkw);
+        we = 1;
+        data_in = i;
+        
+    end
+    endtask
+
+    task read(); begin
+        @(negedge clkr);
+        re = 1;
+        we = 0;
+        @(negedge clkr);
+        re = 0;
+    end
+    endtask
+
+    initial begin
+        initialize;
+        write(8'h44);
+        write(8'ha0);
+        write(8'hde);
+        write(8'had);
+        read;
+        write(8'h00);
+        write(8'h10);
+        write(8'ha0);
+        read;
+        read;
+        read;
+        read;
+        write(8'hfe);
+        write(8'he1);
+        write(8'h44);
+        read;
+        read;
+        read;
+        read;
+        write(8'hbe);
+        write(8'h9f);
+        write(8'h00);
+        read;
+        read;
+        read;
+        read;
+        $finish;
+        
+    end
+
+endmodule  
